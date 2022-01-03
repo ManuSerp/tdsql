ec85828118de94aef48b201aa884ba1327be3e09
DatBen <benjamindumont@live.com>
2022-01-03 16:15:09 +0100

    update

--- remplissage.sql
+++ remplissage.sql
@@ -8,8 +8,6 @@ INSERT INTO `Classes`(`ClassID`,`Enseignant`) VALUES
 (1,'Cotillon'),
 (2,'Dumont');
 
-INSERT INTO `Repartition`(`ElevID`,`ActID`) VALUES
-(0,0),(1,0),(2,1),(3,1),(4,1),(5,2);
 
 INSERT INTO `Eleves`(`ElevID`,`Nom`,`Age`,`Ville`,`ClassID`) VALUES
 (0,'Robin',15,'Metz',0),
@@ -18,4 +16,7 @@ INSERT INTO `Eleves`(`ElevID`,`Nom`,`Age`,`Ville`,`ClassID`) VALUES
 (3,'Augustin',17,'Blois',1),
 (4,'Gregoire',9,'Paris',2),
 (5,'Paul',16,'Marseille',2);
+
+INSERT INTO `Repartition`(`ElevID`,`ActID`) VALUES
+(0,0),(1,0),(2,1),(3,1),(4,1),(5,2);
 

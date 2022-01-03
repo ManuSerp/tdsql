341c382c1829a9e53633c76e2537e220ad83aa64
DatBen <benjamindumont@live.com>
2022-01-03 17:10:03 +0100

    update

--- request.sql
+++ request.sql
@@ -3,3 +3,9 @@
 SELECT Enseignant, ListeEleves 
 FROM Classes AS C JOIN (SELECT ClassID, GROUP_CONCAT(Nom) as ListeEleves FROM Eleves GROUP BY ClassID) AS T 
 ON C.ClassID=T.ClassID
+
+/*q4*/
+
+SELECT Jour, GROUP_CONCAT('Bus n°',Bus,' : ',  Nb,' élèves') AS ListeDesBus 
+FROM (SELECT ActID, COUNT(ElevID) as Nb FROM Repartition GROUP BY ActID) as C JOIN Activites AS A ON C.ActID=A.ActID
+GROUP BY Jour

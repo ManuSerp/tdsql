8959e3bdb05a130906d1b5c1b8000b496f61ed0c
DatBen <benjamindumont@live.com>
2022-01-03 16:52:13 +0100

    update

--- remplissage.sql
+++ remplissage.sql
@@ -2,7 +2,7 @@
 INSERT INTO `Activites`(`ActID`,`Lieu`,`Bus`,`Theme`,`Jour`) VALUES
 (0,'Stade',1,'Rugby',1),
 (1,'Piscine',2,'Natation',3),
-(2,'LaMeuh',1,'PAF',6);
+(2,'LaMeuh',1,'PAF',6),
 (3,'CityStadium',2,'Foot',1);
 
 INSERT INTO `Classes`(`ClassID`,`Enseignant`) VALUES

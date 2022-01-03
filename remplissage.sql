fc8a8a78acbd58f8e2d7dd5a741d7b718079e74e
DatBen <benjamindumont@live.com>
2022-01-03 16:12:56 +0100

    repartition

--- remplissage.sql
+++ remplissage.sql
@@ -7,3 +7,6 @@ INSERT INTO `Classes`(`ClassID`,`Enseignant`) VALUES
 (0,'Maria'),
 (1,'Cotillon'),
 (2,'Dumont');
+
+INSERT INTO `Repartition`(`ElevID`,`ActID`) VALUES
+(0,0),(1,0),(2,1),(3,1),(4,1),(5,2);

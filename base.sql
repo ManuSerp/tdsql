e120bd6c9795b304668b747bb21077ac67907659
Serp <manu.serpette74@gmail.com>
2022-01-03 15:35:46 +0100

    eleves

--- base.sql
+++ base.sql
@@ -1,1 +1,8 @@
-
+ 
+CREATE TABLE `Eleves` (
+  `ElevID` int(11) NOT NULL,
+  `Nom` varchar(100) NOT NULL,
+  `Age` int(11) NOT NULL,
+  `Ville` varchar(100) NOT NULL,
+  `ClassID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

ac9a3ca13ca9101f44cb895be40531b75cce9308
Serp <manu.serpette74@gmail.com>
2022-01-06 14:15:41 +0100

    q7

--- request.sql
+++ request.sql
@@ -26,6 +26,14 @@ FROM Eleves as Z JOIN (SELECT ElevID,Jour from Repartition as C JOIN Activites A
 GROUP BY Nom,Jour) AS A
 GROUP BY Nom) AS B WHERE B.Presence = (SELECT COUNT(DISTINCT Jour) From Activites)
 
+/*q7*/
+
+SELECT *
+FROM Eleves as Z JOIN (SELECT ElevID,Jour,Lieu from Repartition as C JOIN Activites AS A ON C.ActID=A.ActID) AS E ON Z.ElevID=E.ElevID
+WHERE Ville=Lieu
+
+
+
 
 /*q8*/
 SELECT Activites,effectif FROM (SELECT GROUP_CONCAT(Theme) AS Activites,effectif,COUNT(effectif) AS ce FROM
